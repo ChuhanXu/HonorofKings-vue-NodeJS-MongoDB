b9ac16c90b999603704d4d5f44c96e0bb8e613c2
xuchuhan777@gmail.com <chx37@pitt.com>
2020-07-18 15:01:12 -0400

    web scss flex setting1

---
+++ web/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <swiper>
+      <swiper-slider>
+        <img class= "w-100" src="../assets/images/20200508164525-463402.jpg">
+
+      </swiper-slider>
+       <swiper-slider>
+        <img class= "w-100" src="../assets/images/20200508165029-541891.jpg">
+
+      </swiper-slider>
+       <swiper-slider>
+        <img class= "w-100" src="../assets/images/20200508174536-924420.jpg">
+
+      </swiper-slider>
+    </swiper>
+  </div>
+    
+</template>
+
---
+++ web/src/views/Main.vue
@@ -0,0 +1,42 @@
+
+<template>
+  <div >
+    <!-- py-2 -->
+    <!-- 主轴方向（vertical）分散对齐。交叉轴（水平方向）垂直居中的，用的ai-center -->
+    <div class="topbar bg-black px-2 py-2 d-flex ai-center" >
+      <img src="../assets/logo.jpg" height="30">
+      <div class="flex-1 text-left ml-2">
+        <div class="text-white">王者荣耀</div>
+        <div class="text-dark-1 fs-xxs">团队成就更多</div>
+      </div>
+      <button type="button" class="btn bg-primary my-2">立即下载</button>
+    </div>
+  
+
+
+   
+
+    <div class="nav bg-primary pt-3 pb-2">
+      <!-- 左右没有间距 space-between 左右有间距 space-around  -->
+      <div class="nav d-flex text-white jc-around pb-1 ">
+        <div class='nav-item active'>
+          <router-link class="nav-link" tag="div" to="/">首页</router-link>
+
+        </div>
+
+        <div class='nav-item'>
+          <router-link class="nav-link" tag="div" to="/">攻略中心</router-link>
+
+        </div>
+
+        <div class='nav-item'>
+          <router-link class="nav-link" tag="div" to="/">赛事中心</router-link>
+
+        </div>
+      </div>
+    </div>
+
+    <!-- 子路由的一个容器 -->
+    <router-view></router-view>
+  </div>
+</template>

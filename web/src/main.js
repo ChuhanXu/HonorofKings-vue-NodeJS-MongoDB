cef6ffffc0c5abc55e1192518d68752a041564bc
xuchuhan777@gmail.com <chx37@pitt.com>
2020-07-19 00:21:53 -0400

    swiper sprite

--- web/src/main.js
+++ web/src/main.js
@@ -3,13 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-import'./style.scss'
+import'./assets/scss/style.scss'
 import router from './router'
 
 
-// import VueAwesomeSwiper from 'vue-awesome-swiper'
-
-// Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
+import VueAwesomeSwiper from '../node_modules/vue-awesome-swiper/src'
+ 
+// import style
+import '../node_modules/swiper/dist/css/swiper.css'
+ 
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
 
 
 new Vue({
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,20 +1,77 @@
 <template>
   <div>
-    <swiper>
-      <swiper-slider>
-        <img class= "w-100" src="../assets/images/20200508164525-463402.jpg">
-
-      </swiper-slider>
-       <swiper-slider>
-        <img class= "w-100" src="../assets/images/20200508165029-541891.jpg">
-
-      </swiper-slider>
-       <swiper-slider>
-        <img class= "w-100" src="../assets/images/20200508174536-924420.jpg">
-
-      </swiper-slider>
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/20200508164525-463402.jpg" />
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/20200508165029-541891.jpg" />
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/20200508174536-924420.jpg" />
+      </swiper-slide>
+      <div class="swiper-pagination pagination-home text-right px-3 pb-0.5" slot="pagination"></div>
     </swiper>
+
+    <!-- 精灵图标 导航栏 -->
+    <div class="nav-icons bg-white mt-3 text-info text-center pt-3">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+        <div class="nav-item mb-3">
+          <i class="sprite sprite-direction"></i>
+          <div class="py-2">方向</div>
+        </div>
+      </div>
+      <div class="bg-light py-2">
+        <span class="text-center">收起</span>
+      </div>
+    </div>
   </div>
-    
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".pagination-home"
+        }
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+//加上lang="scss"才可以嵌套
+// @import '../style.scss'; 再style中把变量和实现分离
+@import "../assets/scss/variable.scss";
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 0.7;
+    border-radius: 0.1538rem;
+    background: map-get($colors, "grey");
+    &.swiper-pagination-bullet-active {
+      background: map-get($colors, "info"); //info 是定义在variabl.scss的颜色
+    }
+  }
+}
+
+//导航栏
+.nav-icons {
+  border-top:1px solid $border-color;
+  border-bottom: 1px solid $border-color;
+  .nav-item {
+    width: 25%; //一行四个
+    border-right: 1px solid $border-color;
+    &:nth-child(4n) {
+      border-right: none; //4,8没有右边框
+    }
+  }
+}
+</style>
 
